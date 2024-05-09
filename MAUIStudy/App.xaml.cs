ff5a1f10fefbede2ca9fdca53ab64420f34229f6
InhoKim <abyz45@mgame.com>
2024-05-09 15:25:36 +0900

    first commit

---
+++ MAUIStudy/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUIStudy
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MAUIStudy/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUIStudy
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
